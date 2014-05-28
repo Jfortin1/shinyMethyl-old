00203bcb63bd0186519187b603988c21dd517cea
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2014-05-28 08:58:11 -0400

    removed runExamplTCGA

--- R/runExampleTCGA.R
+++
@@ -1,5 +0,0 @@
-runExampleTCGA <- function(){
-	summary.tcga.raw <- get(data("summary.tcga.raw", package = "shinyMethylData"))
-	summary.tcga.norm <- get(data("summary.tcga.norm", package = "shinyMethylData"))
-	runShinyMethyl(summary.tcga.raw, summary.tcga.norm)
-}
